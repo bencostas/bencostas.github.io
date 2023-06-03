43cdccc4380206bad040d883ff5c793ff39fcef3
Ben Costas <bencostas@gmail.com>
2023-06-02 21:25:16 -0400

    Updating jobdata.js

--- src/Components/JobData.js
+++ src/Components/JobData.js
@@ -4,11 +4,10 @@ const Jobs =[
         title: "Full Stack Developer",
         date: "July 2022 - Sept. 2022",
         desc: [
-            "Utilized MERN stack to develop and implement new features and functionalities for an internal government financial data collection tool. Used by 1000+ health institutions across Ontario.",
+            "Leveraged MERN stack to enhance product usability for 100+ health institutions across Ontario by developing and implementing new features and functionalities for an internal government financial data collection tool.",
             "Improved scalability and user experience by using TypeScript and React to design and revamp 15+ front-end components.",
-            "Built backend REST APIs using Express, Node, Mongoose, and MongoDB for user data storage, retrieval, and parsing.",
-            "Followed Agile processes to deliver regular updates in short development sprints. Attended daily standups, produced design documents for components, and prepared presentations for code demonstrations.",
-            "Worked in a team of 4 developers. Employed Azure DevOps to view work items, code collaboration, and running pipelines."
+            "Built 7+ backend REST APIs using Express.js and MongoDB to correct software functionality by implementing CRUD operations including user data storage, retrieval, mapping, and parsing, improving data entry speed by 80%.",
+            "Collaborated with developers and business executives by utilizing Azure DevOps for efficient management of work items, seamless code collaboration, and streamlined pipeline execution."
         ]
     },
     {
@@ -16,18 +15,18 @@ const Jobs =[
         title: "Systems Developer",
         date: "Sept. 2022 - Apr. 2023",
         desc: [
-            "Iterated new designs and followed wireframe documents to improve user experience by refining 5 pages in an internal government portal client using Figma and JavaScript. Used by 1000+ financial services companies across Canada.",
-            "Created reverse web proxies to onboard and connect external applications and services to the government portal. Configured firewalls and whitelisted external users to improve security measures in the system architecture.",
-            "Refactored the codebase by externalizing 100+ hard-coded variables, functions, and expressions. Resulted in significant improvements in testing efficiency and less manual intervention in production environments.",
-            "Organized and performed an SQL dump to successfully transfer data and code amongst 2 data centers. Communicated with external partners to ensure testing and functionalities of applications affected by the dump",
-            "Worked with 2 teams alongside 20+ developers and specialists. Attended daily scrum meetings and weekly sprint plannings; participating in code reviews, code promotions, solutioning, and testing."
+            "Iterated new designs and followed wireframe documents to elevate user experience for 1000+ financial services companies across Canada by refining 5 pages in an internal government portal client using Figma and JavaScript.",
+            "Created reverse web proxies to connect external applications/services to the government portal. Integrated Broadcom Enterprise Software to improve system infrastructure security by configuring firewalls and whitelisting external users.",
+            "Refactored the codebase by externalizing 100+ hard-coded variables, functions, and expressions. Resulted in an increase in efficiency, prevented potential losses in revenue, and reduced manual configuration in production environments.",
+            "Organized and performed a large-scale SQL dump to transfer over 50GB of data and code between 2 data centers. Collaborated with external partners to ensure seamless integration and testing of impacted applications.",
+            "Worked closely with 2 teams comprising of 20+ developers, specialists, and business executives. Contributed to daily scrum meetings and weekly sprint plannings by actively engaging in code reviews, solutioning, and testing."
         ]
     },
     {
         company: "Kumon",
         title: "Software Developer",
         date: "May 2023 - Aug. 2023",
-        desc: ["Incoming Software Developer at Kumon"
+        desc: ["Developed a large-scale data storage system application using Django and MS SQL, handling over 400+ critical client information and operated by over 30 users, reducing average data entry speed from 30 seconds to 5 seconds"
         ]
     }
 ]
