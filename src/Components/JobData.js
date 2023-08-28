7d0e9a8b414798b66dcd298a21e5f1c29a3605c5
Ben Costas <bencostas@gmail.com>
2023-08-27 22:18:26 -0400

    updated job data

--- src/Components/JobData.js
+++ src/Components/JobData.js
@@ -4,10 +4,9 @@ const Jobs =[
         title: "Full Stack Developer Intern",
         date: "July 2022 - Sept. 2022",
         desc: [
-            "Leveraged MERN stack to enhance product usability for 100+ health institutions across Ontario by developing and implementing new features and functionalities for an internal government financial data collection tool.",
-            "Improved scalability and user experience by using TypeScript and React to design and revamp 15+ front-end components.",
-            "Built 7+ backend REST APIs using Express.js and MongoDB to correct software functionality by implementing CRUD operations including user data storage, retrieval, mapping, and parsing, improving data entry speed by 80%.",
-            "Collaborated with developers and business executives by utilizing Azure DevOps for efficient management of work items, seamless code collaboration, and streamlined pipeline execution."
+            "Improved scalability and user experience by using TypeScript and React to implement new features for an internal government financial data collection tool. Used by 100+ health institutions across Ontario",
+            "Optimized app performance and corrected software functionality by building 7+ backend REST APIs using Express.js and MongoDB, integrating CRUD functions to extract and parse Excel sheet data to the client",
+            "Collaborated with developers and business executives by utilizing Azure DevOps for efficient management of work items, seamless code collaboration, and streamlined CI/CD pipeline execution."
         ]
     },
     {
@@ -15,18 +14,20 @@ const Jobs =[
         title: "Systems Developer Intern",
         date: "Sept. 2022 - Apr. 2023",
         desc: [
-            "Iterated new designs and followed wireframe documents to elevate user experience for 1000+ financial services companies across Canada by refining 5 pages in an internal government portal client using Figma and JavaScript.",
-            "Created reverse web proxies to connect external applications/services to the government portal. Integrated Broadcom Enterprise Software to improve system infrastructure security by configuring firewalls and whitelisting external users.",
-            "Refactored the codebase by externalizing 100+ hard-coded variables, functions, and expressions. Resulted in an increase in efficiency, prevented potential losses in revenue, and reduced manual configuration in production environments.",
-            "Organized and performed a large-scale SQL dump to transfer over 50GB of data and code between 2 data centers. Collaborated with external partners to ensure seamless integration and testing of impacted applications.",
-            "Worked closely with 2 teams comprising of 20+ developers, specialists, and business executives. Contributed to daily scrum meetings and weekly sprint plannings by actively engaging in code reviews, solutioning, and testing."
+            "Enhanced system security, authentication, and integrity by configuring API Gateways within a government portal using XML and JavaScript. Elevated user experience for 1000+ financial services firms across Canada.",
+            "Created reverse web proxies to connect external applications/services onto the API gateways. Integrated Broadcom Enterprise Software to improve infrastructure security by configuring firewalls and whitelisting external users.",
+            "Restructured the codebase by resolving 30+ coding faults including hard-coded variables, functions, and RegEx. Improved code reusability, system reliability, and streamlined manual setup in software development environments.",
+            "Organized and performed a large-scale SQL dump to migrate over 50GB of data and code between 2 data centers. Communicated with external partners to ensure seamless integration and testing of impacted applications."
         ]
     },
     {
         company: "Kumon",
         title: "Software Developer Intern",
         date: "May 2023 - Aug. 2023",
-        desc: ["Developed a large-scale data storage system application using Django and MS SQL, handling over 400+ critical client information and operated by over 30 users, reducing average data entry speed from 30 seconds to 5 seconds"
+        desc: [
+            "Spearheaded the development of a large-scale data storage web application using Django and MS SQL. Managing over 500+ critical client information and operated by over 30 users, reducing average data entry speed by 250%.",
+            "Enhanced user experience and interaction by integrating JavaScript and Bootstrap to construct a dynamic user interface. Ensured long-term scalability and maintainability by creating reusable components.",
+            "Implemented 5 backend REST APIs enabling CRUD operations between the client and database. Resolved issues in SQL query definitions boosting data accuracy by 10%, improving the overall efficiency and reliability of the application."
         ]
     }
 ]
